b10e31a87ccb67b5d147f2452831a32a7f7ec69c
asier <alualb1634@ieselcaminas.org>
2022-10-06 13:04:25 +0200

    Reformat commit

--- src/Example22.java
+++ src/Example22.java
@@ -2,7 +2,7 @@ public class Example22 {
     public static void main(String[] args) {
         int sum = 0, prod = 1;
 
-        for (int i = 1; i <= 10; i++){
+        for (int i = 1; i <= 10; i++) {
             sum = sum + i;
             prod = prod * i;
         }
