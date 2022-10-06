b39733a98ca682cbe260c5a2f1d9ddea2d20a8f4
asier <alualb1634@ieselcaminas.org>
2022-10-06 13:04:04 +0200

    Initial commit

---
+++ src/Example22.java
@@ -0,0 +1,12 @@
+public class Example22 {
+    public static void main(String[] args) {
+        int sum = 0, prod = 1;
+
+        for (int i = 1; i <= 10; i++){
+            sum = sum + i;
+            prod = prod * i;
+        }
+        System.out.println("Addition = " + sum);
+        System.out.println("Product = " + prod);
+    }
+}
